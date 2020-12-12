aa7616955f375c0d6ff522f844d14d2fee366596
Wendy C <chriscowendy@gmail.com>
2020-12-12 11:48:06 -0800

    created the array that holds all the images

---
+++ js/magic8Ball.js
@@ -0,0 +1,4 @@
+let imagesArray = ['back1.svg', 'back2.svg', 'back3.svg',
+                'back4.svg', 'back5.svg', 'back6.svg',
+                'back7.svg', 'back8.svg', 'back9.svg',
+                'back10.svg', 'back11.svg', 'back12.svg',];

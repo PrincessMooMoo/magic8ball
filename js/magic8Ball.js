ccaa035875ccab833efc1bee05dc152ad92b54f0
Wendy C <chriscowendy@gmail.com>
2020-12-12 12:12:39 -0800

    created the userInput variable

--- js/magic8Ball.js
+++ js/magic8Ball.js
@@ -1,4 +1,13 @@
-let imagesArray = ['back1.svg', 'back2.svg', 'back3.svg',
-                'back4.svg', 'back5.svg', 'back6.svg',
-                'back7.svg', 'back8.svg', 'back9.svg',
-                'back10.svg', 'back11.svg', 'back12.svg',];
+const imagesArray = ['back1.svg', 'back2.svg', 'back3.svg',
+    'back4.svg', 'back5.svg', 'back6.svg',
+    'back7.svg', 'back8.svg', 'back9.svg',
+    'back10.svg', 'back11.svg', 'back12.svg',
+    'back13.svg', 'back14.svg', 'back15.svg',
+    'back16.svg', 'back17.svg', 'back18.svg',
+    'back19.svg', 'back20.svg'];
+
+function onSubmit(event) {
+    event.preventDefault();
+    const userInput = event.target.userInput.value;
+    console.log(userInput);
+}

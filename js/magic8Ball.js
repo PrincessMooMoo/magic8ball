1a2eae1ed1c2c07c7c400c5ea86bcea9b1d07cff
Wendy C <chriscowendy@gmail.com>
2020-12-14 08:19:30 -0800

    got the question that was inputted by the user to display

--- js/magic8Ball.js
+++ js/magic8Ball.js
@@ -1,11 +1,3 @@
-// const imagesArray = ['back1.svg', 'back2.svg', 'back3.svg',
-//     'back4.svg', 'back5.svg', 'back6.svg',
-//     'back7.svg', 'back8.svg', 'back9.svg',
-//     'back10.svg', 'back11.svg', 'back12.svg',
-//     'back13.svg', 'back14.svg', 'back15.svg',
-//     'back16.svg', 'back17.svg', 'back18.svg',
-//     'back19.svg', 'back20.svg'];
-
 const imagesArray = ['1', '2', '3', '4', '5', '6', '7', 
                     '8', '9', '10', '11', '12', '13', '14',
                     '15', '16', '17', '18', '19', '20'];
@@ -14,14 +6,16 @@ const title = document.querySelector('.header');
 const imagePlaceholder = document.querySelector('#firstImage');
 const form = document.querySelector('.form-layout');
 const image = document.querySelector('.image');
+const displayQuestion = document.querySelector(".display-question");
 
 function onSubmit(event) {
     event.preventDefault();
     const userInput = event.target.userInput.value;
+    console.log(userInput);
     title.style.display = 'none';
     image.style.display = 'none';
     form.style.display = 'none'; 
-
+    return userInput;
 }
 
 function displayRandomImage(imagePlaceholder){
@@ -33,3 +27,7 @@ function displayRandomImage(imagePlaceholder){
         answerImage.appendChild(newImage);
     }
 }
+
+function displayInput(userInput){
+    displayQuestion.innerHTML = userInput.value;
+};
